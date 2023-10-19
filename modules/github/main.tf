c7420486416744400c257deab2a65bd8ae6f71e5
Will Sargent <109557847+will-sargent-dbtlabs@users.noreply.github.com>
2023-10-19 08:50:07 -0600

    adds hidden files to initial repo

--- modules/github/main.tf
+++ modules/github/main.tf
@@ -54,7 +54,8 @@ resource "null_resource" "post_repo_creation" {
       git init
       git config user.email "nomail@dbt.com"
       git config user.name "Created by Terraform"
-      git add *
+      git add */.*
+      git rm -f .cruft.json
       git commit -m "Initial commit from Terraform and cruft"
       git branch -M main
       git remote add origin ${local.https_url_with_pat}
